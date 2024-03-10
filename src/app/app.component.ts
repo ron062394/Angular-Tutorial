2a31dabbefbfe67976221ca9d068194b12d76bbc
ron062394 <ron062394@gmail.com>
2024-03-10 16:11:26 +0800

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
+// Import statement: Used to import the Component decorator from the '@angular/core' module.
 import { Component } from '@angular/core';
+import { WishItem } from '../shared/models/wishItem';
 
+const filters = [
+  (item : WishItem) => item,
+  (item : WishItem) => !item.isComplete,
+  (item : WishItem) => item.isComplete,
+]
+
+// Decorator: A type of design pattern used for modifying or enhancing the behavior of classes, methods, properties, or parameters.
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  selector: 'app-root', // Selector: Defines the HTML tag that represents this component in the DOM.
+  templateUrl: './app.component.html', // TemplateUrl: Specifies the location of the HTML template file for this component.
+  styleUrls: ['./app.component.css'] // StyleUrls: Specifies the location of the CSS file(s) for styling this component.
 })
+
+
+// AppComponent class: Represents the root component of the Angular application.
 export class AppComponent {
-  title = 'Angular-App';
+  items : WishItem[] = [
+    new WishItem('Learn Angular'),
+    new WishItem('Get Coffee', true),
+    new WishItem('Find grass that cuts it'),    
+  ];
+  title = 'Coding!!!!';
+
+  listFilter : any = '0';
+  
+
+  get visibleItems() : WishItem[] {
+    return this.items.filter(filters[this.listFilter])
+  } ;
+
+
+
+
 }
+
---
+++ src/app/wish-filter/wish-filter.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-wish-filter',
+  templateUrl: './wish-filter.component.html',
+  styleUrl: './wish-filter.component.css'
+})
+export class WishFilterComponent {
+
+}
